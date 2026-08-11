abc6cc1f9401c17a311fa59aac79c59fd7db94d2
Marco Pedroza <mpedroza.developer@gmail.com>
2026-08-10 20:52:04 -0600

    initial commit for spring-boot-multi-module-maven-app-example

---
+++ spring-boot-multi-module-maven-app-example/spring-boot-rest-service/src/main/java/org/learning/spring/spring_boot_rest_service/SpringBootRestServiceApplication.java
@@ -0,0 +1,13 @@
+package org.learning.spring.spring_boot_rest_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestServiceApplication.class, args);
+	}
+
+}
---
+++ spring-boot-multi-module-maven-app-example/spring-boot-rest-service/src/test/java/org/learning/spring/spring_boot_rest_service/SpringBootRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.spring.spring_boot_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
