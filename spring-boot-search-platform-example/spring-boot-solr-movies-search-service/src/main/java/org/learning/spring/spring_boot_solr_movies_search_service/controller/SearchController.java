98e9015bfe46c79fe54880613cb4025f4066fec1
Marco Pedroza <mpedroza.developer@gmail.com>
2026-08-16 14:45:15 -0600

    add test code to trigger prometheus alert

--- spring-boot-search-platform-example/spring-boot-solr-movies-search-service/src/main/java/org/learning/spring/spring_boot_solr_movies_search_service/controller/SearchController.java
+++ spring-boot-search-platform-example/spring-boot-solr-movies-search-service/src/main/java/org/learning/spring/spring_boot_solr_movies_search_service/controller/SearchController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 @RequestMapping("/api/movies/search")
@@ -19,12 +20,20 @@ public class SearchController {
 
    private final SearchService searchService;
 
+   private static final AtomicInteger COUNTER = new AtomicInteger(0);
+
     public SearchController(SearchService searchService) {
         this.searchService = searchService;
     }
 
     @PostMapping
-    public SearchResponseDto searchMovies(@RequestBody SearchRequestDto searchRequestDto) throws SolrServerException, IOException {
+    public SearchResponseDto searchMovies(@RequestBody SearchRequestDto searchRequestDto) throws SolrServerException, IOException, InterruptedException {
+
+        //Simulate delay to activate Prometheus Alert
+//        if (COUNTER.getAndIncrement() % 2 == 0){
+//            Thread.sleep(10000);
+//        }
+
         return searchService.search(searchRequestDto);
     }
 }
