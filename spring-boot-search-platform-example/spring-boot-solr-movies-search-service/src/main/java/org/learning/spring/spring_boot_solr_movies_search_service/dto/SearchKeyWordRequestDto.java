6d12addb821a25d4142868bd8b63c007f535da27
Marco Pedroza <mpedroza.developer@gmail.com>
2026-09-02 17:20:06 -0600

    add SearchKeyWordRequestDto

---
+++ spring-boot-search-platform-example/spring-boot-solr-movies-search-service/src/main/java/org/learning/spring/spring_boot_solr_movies_search_service/dto/SearchKeyWordRequestDto.java
@@ -0,0 +1,18 @@
+package org.learning.spring.spring_boot_solr_movies_search_service.dto;
+
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchKeyWordRequestDto implements SearchRequestDto {
+    private String query;
+    private List<Filterable> filters;
+}
